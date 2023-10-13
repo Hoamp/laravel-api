d9c1d0a92cde97cf3f247526bfac849fc82b51da
Hoamp <setiawanthomas43@gmail.com>
2023-10-13 18:54:27 +0700

    add gitur register

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/siswa', SiswaController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
